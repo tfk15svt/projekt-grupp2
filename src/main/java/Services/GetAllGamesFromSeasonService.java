ae94bc6fb3f6313dde13720371e78a0033c0a524
Peeftw <Peeftw@172.16.109.96>
2018-04-05 14:23:35 +0200

    Commit Johan

---
+++ src/main/java/Services/GetAllGamesFromSeasonService.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services;
+
+import Broker.BrokerFactory;
+import Broker.RoundBroker;
+import Broker.SeasonBroker;
+import Domain.Game;
+import Domain.Round;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ *
+ * @author Peeftw
+ */
+public class GetAllGamesFromSeasonService extends Service {
+    private final Long seasonId;
+    private List<Game> gameList;
+    public GetAllGamesFromSeasonService(Long seasonId) {
+        this.seasonId = seasonId;
+        if(seasonId == null){
+            throw new ServiceException ("No round with that id");
+        }
+    }
+    public List<Game> execute (){
+        SeasonBroker seasonBroker = getBrokerFactory().getSeasonBroker();
+        if (seasonBroker.findSeasonById(seasonId) == null){
+            throw new ServiceException("No round with that id");
+        }
+        List<Round> roundList = seasonBroker.getAllRoundsfromSeasonId(seasonId);
+        RoundBroker roundBroker = getBrokerFactory().getRoundBroker();
+        gameList = new ArrayList(); 
+        for(Round round : roundList){
+           List<Game> gamesFromRound = roundBroker.getAllGamesByRoundId(round.getId());
+           gameList = Stream.concat(gameList.stream(), gamesFromRound.stream()).collect(Collectors.toList());
+         
+        }
+        return gameList;   
+        }
+    
+    }
+    
+
---
+++ src/test/java/Services/GetAllGamesFromRoundServiceTest.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services;
+
+import Broker.BrokerFactory;
+import Broker.RoundBroker;
+import Domain.Game;
+import Domain.League;
+import Domain.Round;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ *
+ * @author Peeftw
+ */
+public class GetAllGamesFromRoundServiceTest {
+    private static BrokerFactory brokerFactory;
+    private static RoundBroker roundBroker;
+    private static List<Game> gameList;
+    private static Game gameOne;
+    private static Round round;
+    
+    public GetAllGamesFromRoundServiceTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        brokerFactory = mock(BrokerFactory.class);
+        roundBroker = mock(RoundBroker.class);
+        round = mock(Round.class);
+        
+        gameOne = mock(Game.class);
+        gameList = new ArrayList<Game>();
+        gameList.add(gameOne);
+        
+        when(brokerFactory.getRoundBroker()).thenReturn(roundBroker);
+        when(roundBroker.getAllGamesByRoundId(1L)).thenReturn(gameList);
+        when(roundBroker.findById(1L)).thenReturn(round);
+    }
+    
+    @Test
+    public void testConstructor(){
+        try {
+            new GetAllGamesFromRoundService(null);
+            fail();
+            
+        } catch (ServiceException e){
+            
+        } 
+        new GetAllGamesFromRoundService(1L);
+    }
+  
+    @Test
+    public void testInit(){
+        GetAllGamesFromRoundService instance = new GetAllGamesFromRoundService(1L);
+        try {
+            instance.init(null);
+            fail();
+           
+        }catch (ServiceException e) {
+            
+        }
+        instance.init(brokerFactory);
+    }
+    
+    @Test
+    public void testExecute() {
+        System.out.println("execute test: ");
+        GetAllGamesFromRoundService instance = new GetAllGamesFromRoundService(1L);
+        instance.init(brokerFactory);
+        List<Game> lista = instance.execute();
+        assertEquals(gameList, lista);
+        
+              
+    }
+    
+}
