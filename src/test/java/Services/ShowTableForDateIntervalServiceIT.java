c55ddaaff77bf2461cb21d71c8462cefa18d3954
Veiret <Veiret@130.243.2.46>
2018-04-17 10:18:25 +0200

    Merge origin/master
    Conflicts:
    	src/main/java/Endpoints/SparkEndPoint.java

---
+++ src/test/java/Services/ShowTableForDateIntervalServiceIT.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Veiret
+ */
+public class ShowTableForDateIntervalServiceIT {
+    
+    public ShowTableForDateIntervalServiceIT() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of execute method, of class ShowTableForDateIntervalService.
+     */
+    @Test
+    public void testExecute() {
+        System.out.println("execute");
+        ShowTableForDateIntervalService instance = null;
+        String expResult = "";
+        String result = instance.execute();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
